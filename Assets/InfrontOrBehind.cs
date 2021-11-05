ee497f1581e73ea42260914cdcf1d242a4fbe32b
Alex Noles <alexnoles@outlook.com>
2021-11-05 20:57:34 +0000

    Script to dynamically put things behind or infront of player

---
+++ Assets/InfrontOrBehind.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfrontOrBehind : MonoBehaviour
+{
+    private SpriteRenderer sr;
+    public float relativePassPoint;
+    public GameObject player; //This should be changed to a static instance once the player movement is implemented
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+
+    void Update()
+    {
+        if (player.transform.position.y > transform.position.y + relativePassPoint)
+        {
+            sr.sortingLayerName = "Infront";
+        }
+        else
+        {
+            sr.sortingLayerName = "Behind";
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(new Vector3(transform.position.x - 100, transform.position.y + relativePassPoint, 0), 
+            new Vector3(transform.position.x + 100, transform.position.y + relativePassPoint, 0));
+    }
+}
