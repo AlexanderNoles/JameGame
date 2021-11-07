e0f3f7e61b884a74f0f6928d760afaee874852c5
Alex Noles <alexnoles@outlook.com>
2021-11-07 18:37:00 +0000

    Added current task indicator

--- Assets/Scripts/TaskScript.cs
+++ Assets/Scripts/TaskScript.cs
@@ -6,17 +6,20 @@ public class TaskScript : MonoBehaviour
 {
     private InteractControl ic;
     public TaskControl.PossibleTasks task;
+    public GameObject icon;
     public GameObject taskObject;   //This should control the specific task through "OnEnable" and "OnDisable"
 
     private void Start()
     {
         ic = GetComponent<InteractControl>();
-        taskObject.SetActive(false);
+        //taskObject.SetActive(false);
+        icon.SetActive(false);
     }
 
     private void Update()
     {
         ic._enabled = TaskControl.currentTasks.Contains(TaskControl.PossibleTasks.VentSteam);
+        icon.SetActive(ic._enabled);
         if(ic.inRange && Input.GetKeyDown(KeyCode.E))
         {
             taskObject.SetActive(true);
